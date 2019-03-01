8960b2c27b3f6ffc44c6f7c3903339ac32afa1cd
Mathias Zinnen <mathias.zinnen@gmail.com>
2019-03-01 11:37:04 +0000

    remove old user ui

--- src/userservice/module.ts
+++ src/userservice/module.ts
@@ -1,46 +1,20 @@
 import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
-import User from './user';
-import * as UserRestService from './userRest';
-
+import Keycloak from 'keycloak-js';
 @Module({ namespaced: true })
 export default class UserModule extends VuexModule {
-  private users: User[] = [];
-  private isLoadingUsers: boolean = true;
-  private roles: string[] = [];
+  private keycloak: Keycloak.KeycloakInstance = Keycloak();
+  private authenticated: boolean = false;
 
-  @Mutation public setUsers(users: User[]) {
-    this.users = users;
-    this.isLoadingUsers = false;
+  @Action({ commit: 'initKeycloak' })
+  public initKeycloakAction() {}
+
+  @Mutation public initKeycloak() {
+    this.keycloak
+      .init({ onLoad: 'login-required' })
+      .success(authenticated => (this.authenticated = authenticated));
   }
 
-  @Mutation public setIsLoadingUsers(value: boolean) {
-    this.isLoadingUsers = value;
-  }
-
-  @Action({ commit: 'setUsers' })
-  public async loadUsers() {
-    this.context.commit('setIsLoadingUsers', true);
-    return await UserRestService.getAllUsers();
-  }
-
-  @Action({ commit: 'setUsers' })
-  public async createUser(user: User) {
-    await UserRestService.addUser(user);
-    return await UserRestService.getAllUsers();
-  }
-
-  @Action({ commit: 'setUsers' })
-  public async deleteUser(userID: string) {
-    await UserRestService.deleteUserById(userID);
-    return await UserRestService.getAllUsers();
-  }
-
-  @Mutation public setRoles(roles: string[]) {
-    this.roles = roles;
-  }
-
-  @Action({ commit: 'setRoles' })
-  public async loadRoles() {
-    return await UserRestService.getAllRoles();
+  get clientId() {
+    return this.keycloak.clientId;
   }
 }
