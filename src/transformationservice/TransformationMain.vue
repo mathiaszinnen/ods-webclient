5ded55f158c9c6eaa5cd175456a0935a6ae1568d
Mathias Zinnen <mathias.zinnen@gmail.com>
2019-03-05 14:06:18 +0000

    basic transformation service layout

--- src/transformationservice/TransformationMain.vue
+++ src/transformationservice/TransformationMain.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="transformation-main">
     <h1>transformation service</h1>
+    <v-card>
+      <v-form>
+        <v-textarea v-model="bla" full-width></v-textarea>
+      </v-form>
+    </v-card>
+    <v-btn color="success">submit</v-btn>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-export default class TransformationMain extends Vue {}
+import Component from 'vue-class-component';
+@Component
+export default class TransformationMain extends Vue {
+  private bla: string = '{"func":"return data", "data":{"bla"}}';
+}
 </script>
