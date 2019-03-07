45e4145d766e677f69a39816fc86036b9e460e76
Mathias Zinnen <mathias.zinnen@gmail.com>
2019-03-07 11:30:11 +0000

    implement communication with transformationservice

--- src/transformationservice/TransformationMain.vue
+++ src/transformationservice/TransformationMain.vue
@@ -6,18 +6,26 @@
         <v-textarea v-model="functionInput" full-width></v-textarea>
       </v-form>
     </v-card>
-    <v-btn color="success">submit</v-btn>
+    <v-btn color="success" @click="submit">submit</v-btn>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { Action } from 'vuex-class';
 
 const namespace = { namespace: 'transformation' };
 
 @Component
 export default class TransformationMain extends Vue {
-  private functionInput: string = '{"func":"return data", "data":{"bla"}}';
+  @Action('fetchTransformation', namespace)
+  private fetchTransformationAction!: (functionInput: string) => void;
+
+  private functionInput: string = '{"func":"return 1", "data":null}';
+
+  private submit() {
+    this.fetchTransformationAction(this.functionInput);
+  }
 }
 </script>
--- src/transformationservice/module.ts
+++ src/transformationservice/module.ts
@@ -1,4 +1,31 @@
-import { Module, VuexModule } from 'vuex-module-decorators';
+import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
+
+const TRANSFORMATION_URL = 'http://localhost:4000/job';
 
 @Module({ namespaced: true })
-export default class TransformationModule extends VuexModule {}
+export default class TransformationModule extends VuexModule {
+  private transformedObject: string = '';
+
+  @Action
+  public fetchTransformation(functionInput: string) {
+    // transformationrestservice.job
+    const job = {
+      func: 'return 1',
+      data: null,
+    };
+    fetch(TRANSFORMATION_URL, {
+      method: 'POST',
+      mode: 'cors',
+      body: functionInput,
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(json => this.context.commit('setTransformedObject', json));
+  }
+
+  @Mutation public setTransformedObject(object: string) {
+    this.transformedObject = object;
+  }
+}
